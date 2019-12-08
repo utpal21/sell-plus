861c062361519d0c0ad8fae1d88ce4362e0cfc41
utpal <utpal.uoda@gmail.com>
2019-12-08 18:30:36 +0600

    user reister

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['namespace' => 'admin'], function() {
-    Route::get('/home', 'DashboardController@index');
+    Route::get('/home'
+    , 'DashboardController@index');
 
 });
+
+Route::get('login', 'admin\AuthController@index');
+Route::post('post-login', 'admin\AuthController@postLogin'); 
+Route::get('registration', 'admin\AuthController@registration');
+Route::post('post-registration', 'admin\AuthController@postRegistration'); 
+Route::get('dashboard', 'admin\AuthController@dashboard'); 
+Route::get('logout', 'admin\AuthController@logout');
+
 //Route::get('/home', 'admin\HomeController@home');
 
