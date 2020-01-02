11ddb254580f34551715dc1b4181a066e3bb8574
utpal <utpal.uoda@gmail.com>
2020-01-02 23:52:23 +0600

    add settings service provider

--- config/app.php
+++ config/app.php
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * custome setting service provider
+         */
+        App\Providers\SettingServiceProvider::class
 
     ],
 
