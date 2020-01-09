cd0b8dc657e21e4e596d465e5f27319bbace435d
utpal <utpal.uoda@gmail.com>
2020-01-10 00:25:35 +0600

    cateory list create

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,9 +26,40 @@ class CategoryController extends BaseController
     public function index()
     {
         $categories = $this->categoryRepository->listCategories();
-        
 
         $this->setPageTitle('Categories', 'List of all categories');
         return view('admin.categories.index', compact('categories'));
+    }
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $categories = $this->categoryRepository->listCategories('id', 'asc');
+
+        $this->setPageTitle('Categories', 'Create Category');
+        return view('admin.categories.create', compact('categories'));
+    }
+    /**
+    * @param Request $request
+    * @return \Illuminate\Http\RedirectResponse
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->createCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while creating category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category added successfully', 'success', false, false);
     }
 }
